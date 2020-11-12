eeb3a171ce290d6e83786b9e7a186d5d2a38c4be
Onur Yüksektepeli <onuryuksektepeli@hotmail.com.tr>
2020-11-12 14:04:42 +0300

    Initial commit

---
+++ Assets/Scripts/DualAxis.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DualAxis : MonoBehaviour
+{
+    public float range;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        float xPos = h * range;
+        float yPos = v * range;
+
+        transform.position = new Vector3(xPos, yPos, 0);
+        Debug.Log("Horizontal: " + h.ToString("F2") +"\nVertical: " + v.ToString("F2"));
+    }
+}
